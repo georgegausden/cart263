003d61a3b5057d4f8f795138308398c188dc5e43
georgegausden <george.philip.gausden@gmail.com>
2022-01-06 18:06:48 -0500

    completed the sausage dog activity tutorial

---
+++ activities/wheres-sausage-dog/js/SausageDog.js
@@ -0,0 +1,23 @@
+class SausageDog extends Animal{
+  constructor(x,y,image){
+    super(x,y,image);
+    this.found = false;
+    this.rotationSpeed = 0.25;
+  }
+
+
+  update(){
+    super.update();
+
+    if (this.found){
+      this.angle += this.rotationSpeed;
+    }
+  }
+
+  mousePressed(){
+    //set the bounds of the sausage dog image
+    if (this.overlap(mouseX,mouseY)){
+      this.found = true;
+    }
+  }
+}
--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -8,11 +8,28 @@ author, and this description to match your project!
 
 "use strict";
 
+//declare global constants
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage;
+let sausageDog;
+
 
 /**
 Description of preload
 */
 function preload() {
+
+  for (let i = 0; i<NUM_ANIMAL_IMAGES; i++){
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+
+  sausageDogImage = loadImage('assets/images/sausage-dog.png');
 
 }
 
@@ -21,6 +38,21 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+
+  //create the animals
+  for (let i = 0; i<NUM_ANIMALS; i++){
+    let x = random(0,width);
+    let y = random(0,height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x,y,animalImage);
+    animals.push(animal);
+  }
+
+  let x = random(0,width);
+  let y = random(0,height);
+  sausageDog = new SausageDog(x,y,sausageDogImage);
 
 }
 
@@ -29,5 +61,17 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(255,255,0);
 
+  for (let i = 0; i<animals.length; i++){
+    let animal = animals[i];
+
+    animal.update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed(){
+  sausageDog.mousePressed();
 }
