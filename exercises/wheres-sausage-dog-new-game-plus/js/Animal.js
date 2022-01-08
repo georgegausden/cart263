fa5dfa2fd7b87e88c909369e9b1b3b33e161198a
georgegausden <george.philip.gausden@gmail.com>
2022-01-08 08:21:01 -0500

    Ex1: added descriptions for my other js files, used beautify

--- exercises/wheres-sausage-dog-new-game-plus/js/Animal.js
+++ exercises/wheres-sausage-dog-new-game-plus/js/Animal.js
@@ -1,54 +1,53 @@
-class Animal{
-  constructor(x,y,image){
+class Animal {
+  constructor(x, y, image) {
     this.x = x;
     this.y = y;
     this.image = image;
     this.maxSpeed = 1
-    this.vx = random(-this.maxSpeed,this.maxSpeed);
-    this.vy = random(-this.maxSpeed,this.maxSpeed);
-
-    this.angle = 0;
+    this.vx = random(-this.maxSpeed, this.maxSpeed);
+    this.vy = random(-this.maxSpeed, this.maxSpeed);
   }
 
-  update(){
+  //updates the position and checks that the animal has left the canvas
+  update() {
     this.display();
     this.move();
     this.wrap();
   }
 
-  display(){
+  //displays the animal on the canvas as an image
+  display() {
     push();
     imageMode(CENTER);
-    translate(this.x,this.y);
-    rotate(this.angle);
-    image(this.image,0,0);
+    translate(this.x, this.y);
+    image(this.image, 0, 0);
     pop();
   }
 
-  overlap(x,y){
-    if (x > this.x - this.image.width/2 && x < this.x + this.image.width/2 && y > this.y - this.image.height/2 && y < this.y + this.image.height/2){
+  //used in sausage dog class to see if the user's mouse is touching the sausage dog image
+  overlap(x, y) {
+    if (x > this.x - this.image.width / 2 && x < this.x + this.image.width / 2 && y > this.y - this.image.height / 2 && y < this.y + this.image.height / 2) {
       return true
     }
   }
 
-  move(){
+  //moves the animal along the canvas
+  move() {
     //make each animal move along the screen fairly slowly.
-    this.x += this.vx * (level+1);
-    this.y += this.vy * (level+1);
+    this.x += this.vx * (level + 1);
+    this.y += this.vy * (level + 1);
   }
 
-  wrap(){
+  //checks to see that the animal left the canvas. If so, moves the animal to the other side
+  wrap() {
     //wrap the animal so they don't leave the frame
-    if (this.x - this.image.width/2 > width){
+    if (this.x - this.image.width / 2 > width) {
       this.x = 0;
-    }
-    else if (this.x + this.image.width/2 < 0){
+    } else if (this.x + this.image.width / 2 < 0) {
       this.x = width;
-    }
-    else if (this.y - this.image.height/2 > height){
+    } else if (this.y - this.image.height / 2 > height) {
       this.y = 0;
-    }
-    else if (this.y + this.image.height/2 < 0){
+    } else if (this.y + this.image.height / 2 < 0) {
       this.y = height;
     }
   }
--- exercises/wheres-sausage-dog-new-game-plus/js/SausageDog.js
+++ exercises/wheres-sausage-dog-new-game-plus/js/SausageDog.js
@@ -1,35 +1,33 @@
-class SausageDog extends Animal{
-  constructor(x,y,image){
-    super(x,y,image);
+class SausageDog extends Animal {
+  constructor(x, y, image) {
+    super(x, y, image);
     this.found = false;
-    this.rotationSpeed = 0.25;
   }
 
-
-  update(){
+  //same as in animal, except add sound effects when the sausage dog is found
+  update() {
     super.update();
 
-    if (this.found){
-      if (!dogFoundSFX.isPlaying()){
+    if (this.found) {
+      if (!dogFoundSFX.isPlaying()) {
         dogFoundSFX.play();
       }
 
-      if (level<WINNING_LEVEL){
-        this.angle += this.rotationSpeed;
+      if (level < WINNING_LEVEL) {
         level += 1;
         resetGame();
         state = 'levelDisplay';
-      }
-      else{
+      } else {
         state = 'end';
       }
 
     }
   }
 
-  mousePressed(){
+  //defines what happens when the mouse is pressed
+  mousePressed() {
     //set the bounds of the sausage dog image
-    if (this.overlap(mouseX,mouseY)){
+    if (this.overlap(mouseX, mouseY)) {
       this.found = true;
     }
   }
