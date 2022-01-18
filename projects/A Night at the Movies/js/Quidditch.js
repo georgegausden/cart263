b3189c8b2c3f62740243d3d350e4234101d64463
georgegausden <george.philip.gausden@gmail.com>
2022-01-18 16:17:43 -0500

    P1: added a new quidditch file to create the quidditch game

---
+++ projects/A Night at the Movies/js/Quidditch.js
@@ -0,0 +1,13 @@
+//the code for the quidditch game
+
+let player = {
+  x: undefined,
+  y: undefined,
+}
+
+//function that calls all the functions in the quidditch game, loads the game
+function playQuidditch(){
+
+}
+
+function
--- projects/A Night at the Movies/js/script.js
+++ projects/A Night at the Movies/js/script.js
@@ -1,9 +1,8 @@
 /**
-Title of Project
-Author Name
+A Night at the Movies
+George Gausden
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Harry Potter!
 */
 
 "use strict";
