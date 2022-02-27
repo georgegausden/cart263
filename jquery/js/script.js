55df52020e2e32f4ddc5828aa78f47341377f6e4
georgegausden <89808071+georgegausden@users.noreply.github.com>
2022-02-27 11:23:12 -0500

    jquery tutorial

---
+++ jquery/js/script.js
@@ -0,0 +1,29 @@
+// let $mainHeading = $(`#main-heading`);
+// $mainHeading.css(`color`,`#339966`);
+
+// $(`.header`).css(`color`,`#ff0000`);
+
+// let $headers = $(`.header`);
+// $headers.css(`color`,`red`);
+// $headers.css(`background-color`,`black`);
+// $headers.css(`font-size`,`3rem`);
+
+// $(`.header`).css({
+//   "color": "blue",
+//   "background-color": "black",
+//   "font-size": "10rem",
+// })
+
+// $(`#example-span`).text(`a dog`);
+
+// $(`#main-heading`).attr(`contenteditable`, `true`);
+//
+// let $link = $(`#thicc-link`);
+//
+// if ($link.attr(`href`) === `https://thi.cc`){
+//   $link.text(`THICC`);
+// }
+
+// let $p = $(`<p></p>`);
+// $p.text(`Fresh, fresh paragraph!`);
+// $(`#second-section`).append($p);
