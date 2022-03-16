66e42a8a34a4facd8d66ee1efa884f94718270ca
georgegausden <89808071+georgegausden@users.noreply.github.com>
2022-03-16 18:59:43 -0400

    P2: experimenting some more with shapes

--- projects/project2/js/Planet.js
+++ projects/project2/js/Planet.js
@@ -1,20 +1,13 @@
 class Planet{
-  constructor(x,y,z,vx,vy,vz,ax,ay,az,size){
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.vx = vx;
-    this.vy = vy;
-    this.vz = vz;
-    this.ax = ax;
-    this.ay = ay;
-    this.az = az;
+  constructor(size){
+
     this.size = size;
 
   }
 
   display(){
     push();
+    ambientMaterial(200,200,100);
     sphere(this.size,20,20);
     pop();
   }
--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -8,6 +8,10 @@ author, and this description to match your project!
 
 "use strict";
 let angle = 0;
+let planets = [];
+let suns = [];
+let numSuns = 1;
+let numPlanets = 1;
 
 /**
 Description of preload
@@ -26,8 +30,16 @@ function setup() {
 
   //create our planets in the solar system
   for (let i = 0; i<numPlanets; i++){
-    
+    let planet = new Planet(100);
+    planets.push(planet);
   }
+
+  //create the suns in the solar system
+  for (let i = 0; i<numSuns; i++){
+    let sun = new Planet(100);
+    suns.push(sun);
+  }
+  
 }
 
 
@@ -38,7 +50,11 @@ function draw() {
   background(0);
   lights();
   noStroke();
-  rotateY(angle);
-  sphere(100);
-  angle+=0.1;
+
+  //display our planets
+  for (let i = 0; i<planets.length; i++){
+    let planet = planets[i];
+
+    planet.display();
+  }
 }
